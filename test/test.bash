314723c2a2187ea2042ccdc0b6dc9eb7be5fa1f7
Kazuki Nakagawa <s24c1089eq@s.chibakoudai.jp>
2026-01-04 15:26:23 +0900

    修正

--- test/test.bash
+++ test/test.bash
@@ -7,13 +7,16 @@ dir=~
 
 cd $dir/ros2_ws
 
+# 1. 追記: ros2コマンドを使えるようにする
+source /opt/ros/humble/setup.bash
+
 # ビルドと環境読み込み
 colcon build
 source install/setup.bash
 
 # ノードを10秒間実行してログを保存
-# (ros2 run で直接起動します)
-timeout 10 ros2 run ros2_pomodoro pomodoro_timer > /tmp/ros2_pomodoro.log
+# 2. 修正: 末尾に 2>&1 をつける
+timeout 10 ros2 run ros2_pomodoro pomodoro_timer > /tmp/ros2_pomodoro.log 2>&1
 
 # ログの確認
 # 正常に動いていれば "Remaining: 24:59" という文字が出ているはず
