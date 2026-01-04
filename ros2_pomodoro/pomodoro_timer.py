7aab23c5e96a9fb9f0da4efd0703ebe4b0ad8017
Kazuki Nakagawa <s24c1089eq@s.chibakoudai.jp>
2026-01-04 14:02:17 +0900

    Initial commit of pomodoro timer

---
+++ ros2_pomodoro/pomodoro_timer.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2025 Kazuki Nakagawa
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class PomodoroTimer(Node):
+    def __init__(self):
+        super().__init__('pomodoro_timer')
+        self.pub = self.create_publisher(String, 'remaining_time', 10)
+        self.remaining_sec = 25 * 60  # 25分 = 1500秒
+        self.create_timer(1.0, self.timer_callback) # 1秒ごとに実行
+
+    def timer_callback(self):
+        # 分と秒に変換
+        minutes = self.remaining_sec // 60
+        seconds = self.remaining_sec % 60
+
+        # メッセージ作成
+        msg = String()
+        msg.data = f"Remaining: {minutes:02d}:{seconds:02d}"
+
+        self.pub.publish(msg)
+        self.get_logger().info(f"Publish: {msg.data}")
+
+        # カウントダウン（0になるまで）
+        if self.remaining_sec > 0:
+            self.remaining_sec -= 1
+
+def main():
+    rclpy.init()
+    node = PomodoroTimer()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_pomodoro'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kazuking',
+    maintainer_email='s24c1089eq@s.chibakoudai.jp',
+    description='A Pomodoro Timer package for ROS 2',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pomodoro_timer = ros2_pomodoro.pomodoro_timer:main',
+        ],
+    },
+)
